c10d6973bc34a83a2ee0aa60b8620bb3608d8480
DarkMerlyn <alejandrotorresv@outlook.com>
2018-03-01 12:20:14 -0600

    java

---
+++ Emmanuel/alerts.js
@@ -0,0 +1,10 @@
+function mostrarholasinparametro ()
+{
+  alert( "hola chaco" );
+}
+function mostrarholaconparametro (mensaje,nombre)
+{
+  // "Hola-Alex"
+  var alerta = mensaje + "-" + nombre;
+  alert ( alerta );
+}
---
+++ Emmanuel/math.js
@@ -0,0 +1,16 @@
+function addnumbers (a,b){
+  var suma = a + b;
+  return suma;
+}
+function lessnumbers (a,b){
+  var less = a - b;
+  return less;
+}
+function dividenumbers (a,b){
+  var divide = a / b;
+  return divide;
+}
+function multiplynumbers (a,b){
+  var multiply = a * b;
+  return multiply;
+}
