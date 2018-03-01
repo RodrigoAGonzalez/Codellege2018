17b1f6c2c10f9c7aea94ad4c7c1b4b05b294dfa9
orliwolf <orlandosalinascampos@gmail.com>
2018-03-01 12:17:00 -0600

    javascript

---
+++ Orlando Salinas Campos/alerts.js
@@ -0,0 +1,10 @@
+function mostrarholasinparametro ()
+{
+  alert ( "hola Rodri" );
+}
+
+function mostrarholaconparametro (parametro1, parametro2) //hola emmanuel
+{
+  var mensaje = parametro1 + " " + parametro2;
+  alert ( mensaje );
+}
