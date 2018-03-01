34ca933ddb1e3dabcca258637e29df4bba65deec
elen94 <eleelautner@gmail.com>
2018-03-01 12:22:02 -0600

    tareas del jueves

---
+++ elena/java/math.js
@@ -0,0 +1,20 @@
+function adsuma(a,b)
+{
+  var suma= a+b;
+  return suma;
+}
+function adresta(a,b)
+{
+  var resta=a-b;
+  return resta;
+}
+function adivision(a,b)
+{
+  var division=a/b;
+  return division;
+}
+function admultiplicacion(a,b)
+{
+  var multiplicacion=a*b;
+  return multiplicacion;
+}
