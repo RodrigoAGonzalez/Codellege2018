d46cb32ed4c4845dc706810a8109e03035a06f50
hectormartinez19 <hector.martinez19@gmail.com>
2018-03-01 12:40:24 -0600

    tarea javascript

---
+++ Hector/maths.js
@@ -0,0 +1,30 @@
+function addNumbers(a,  b )
+{
+  var suma = a + b;
+
+  return suma;
+}
+
+
+
+function restas(a,b)
+
+{
+  var resta = a - b;
+
+  return resta;
+}
+
+function divisiones(a,b)
+{
+  var division = a / b;
+
+  return division;
+}
+
+function multiplica (a,b)
+{
+  var multiplicacion = a * b;
+
+  return multiplicacion;
+}
